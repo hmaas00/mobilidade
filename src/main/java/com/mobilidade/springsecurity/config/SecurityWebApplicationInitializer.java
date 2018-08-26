03421602946b5b4832ad6421091aabcc01a537f0
harlan maas <hmaas00@gmail.com>
2018-08-26 17:12:40 -0300

    base projeto

---
+++ src/main/java/com/mobilidade/springsecurity/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.mobilidade.springsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
